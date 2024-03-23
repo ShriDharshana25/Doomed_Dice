3ddb89552aea2879ca70cf7c33243526ec2d953d
Dharshana Shri Parthiban <138512593+ShriDharshana25@users.noreply.github.com>
2024-03-23 10:20:28 +0530

    Update Part_B_Problem.py

--- Part_B_Problem.py
+++ Part_B_Problem.py
@@ -1,38 +1,77 @@
-num_sides = 6
-total_combinations = num_sides ** 2
-print("Total combinations when two dice are rolled:", total_combinations)
+Die_A = [1,2,3,4,5,6]
+Die_B = [1,2,3,4,5,6]
 
+print("Original Dice A =", Die_A)
+print("Original Dice B =", Die_B)
+print("\nProbability of Original Dice :\n")
 
-def undoom_dice(die_a, die_b):
-    a = [min(4, spots) for spots in die_a]
-    b = [min(6, spots) for spots in die_b]
-    return a, b
-die_a = [1, 2, 3, 4, 5, 6]
-die_b = die_a
-new_die_a, new_die_b = undoom_dice(die_a, die_b)
-print("")
-print(" Modified Values of the dice      ")
-print("\nNew Die A:", new_die_a)
-print("New Die B:", new_die_b)
+total_combinations = len(Die_A) * len(Die_B)
 
+original_sums = {}
+for i in range(len(Die_A)):
+    for j in range(len(Die_B)):
+        sums = Die_A[i] + Die_B[j]
+        count = original_sums.get(sums,0)+1
+        original_sums[sums] = count
 
-distribution = [[0] * 6 for _ in range(6)]
-for i in range(len(new_die_a)):
-    for j in range(len(new_die_b)):
-        distribution[i][j] = (new_die_a[i] , new_die_b[j])
-print("Distribution of combinations:")
-for row in distribution:
-    print(row)
+for sums,count in original_sums.items():
+    print("P(Sum = ",sums,") Count =",original_sums[sums],"Probability = ",count,"/",total_combinations,"=",round(count/total_combinations,2))
+   
+new_sums = original_sums
+
+diceA = []
+diceB = []
+
+def diceA_possibility(number,t):    
+    if number > 4:
+        return
+    if len(t) > 6:
+        return
+    if len(t) == 6:
+        if t not in diceA:
+            diceA.append(t)
+        return
+    for i in range(number,5):          
+        diceA_possibility(i,t.copy() + [i])
     
+def diceB_possibility(number,t):
+    if number > 11:
+        return
+    if len(t) > 6:
+        return
+    if len(t) == 6:
+        if t not in diceB:
+            diceB.append(t)
+        return
+    for i in range(number+1,13):   
+        diceB_possibility(i,t.copy()+[i])
     
-for sums in range(2, 13):
-    print("\n")
-    count=0
-    for j in range(len(new_die_b)):
-        for i in range(len(new_die_a)):
-            distribution[i][j] = (new_die_a[i] + new_die_b[j])
-            if distribution[i][j]==sums:
-                print("(",new_die_a[i],",",new_die_b[j],")")
-                count+=1
-    probability=count/total_combinations
-    print(f"P(Sum = {sums})  :   {count}/{total_combinations} = {probability:.2f}")
+def undoom_dice():
+    for i in range(1,5):
+        diceA_possibility(i,[i])
+    for j in range(1,12):
+        diceB_possibility(j,[j])
+    for i in diceA:
+        for j in diceB:
+            temp_dict = {}
+            for k in range(len(i)):
+                for l in range(len(j)):
+                    sums=i[k]+j[l]
+                    count = temp_dict.get(sums,0)+1
+                    temp_dict[sums] = count           
+            mc = 0
+            for sums,count in temp_dict.items():
+                Original_count=new_sums.get(sums,-1)
+                if count==Original_count:
+                    mc+=1
+            if mc == 11:
+                return i,j,temp_dict
+                
+New_Die_A,New_Die_B,new_temp_dict = undoom_dice()
+
+print("\nNew Dice A = ",New_Die_A)
+print("New Dice B = ",New_Die_B)
+print("\nProbability of New Dice :\n")
+
+for key,count in new_temp_dict.items():
+    print("P(Sum = ",key,") Count =",original_sums[key],"Probability = ",count,"/",total_combinations,"=",round(count/total_combinations,2))
